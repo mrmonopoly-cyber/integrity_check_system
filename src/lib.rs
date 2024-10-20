c824b9d2e9d5cb7a139e113da1a52bb57ad5a669
Alberto Damo <alberto.damo@gmail.com>
2024-10-20 16:40:38 +0200

    generic reotganization

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,37 @@ use bus::BusComumication;
 use external::ExternalCheck;
 use internal::InternalCheck;
 
-pub trait ICS1<MEX,BUS,I,ERR> 
+pub struct ICSError{
+}
+
+#[allow(unused)]
+pub struct ICS<MEX,INT,BUS> where 
+BUS: BusComumication<MEX>,INT:InternalCheck
+{
+    bus: BUS,
+    int_vec: Vec<INT>,
+    ext_vec: Vec<ExternalCheck<MEX,BUS>>,
+}
+
+pub trait ICST<MEX,BUS,I,ERR> 
 where BUS: BusComumication<MEX>, I: InternalCheck{
-    fn new(node_id: u8,bus :BUS) -> Self;
     fn add_internal_check(&mut self, check: I) -> ();
     fn add_external_check(&mut self, check: ExternalCheck<MEX,BUS>) -> ();
-    fn run_check(& mut self) -> (bool,ERR);
+    fn run_check(& mut self) -> Option<ERR>;
+}
+
+
+#[allow(unused)]
+impl<MEX,BUS,INT> ICST<MEX,BUS,INT,ICSError> for ICS<MEX,INT,BUS>
+where BUS: BusComumication<MEX>, INT: InternalCheck{
+    fn add_internal_check(&mut self, check: INT) -> ()
+    {
+    }
+    fn add_external_check(&mut self, check: ExternalCheck<MEX,BUS>) -> ()
+    {
+    }
+    fn run_check(& mut self) -> Option<ICSError>
+    {
+        None
+    }
 }
