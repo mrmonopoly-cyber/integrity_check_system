e120d7f955be10620e219d42ae5c5a5b40dc830b
Alberto Damo <alberto.damo@gmail.com>
2024-10-20 17:49:43 +0200

    first small implementation to check the internal errors

--- src/internal.rs
+++ src/internal.rs
@@ -1,4 +1,49 @@
-pub trait InternalCheck {
-    fn check_var(&self) -> bool;
-    fn manage_fail(&self) -> ();
+type CheckFn = fn() -> bool;
+type IntErrFn= fn() -> ();
+
+pub enum IntErrStatus{
+    OK,
+    ERR,
+}
+
+#[allow(unused)]
+pub struct InternalCheck{
+    description: String,
+    check :CheckFn,
+    manage_fail: IntErrFn,
+    reset_fail: IntErrFn,
+    status: IntErrStatus,
+}
+
+#[allow(unused)]
+impl InternalCheck{
+    pub fn new(description: String, check: CheckFn, 
+        manage_fail: IntErrFn, reset_fail: IntErrFn) -> Self{
+        Self{description,check,manage_fail,reset_fail,status:IntErrStatus::OK}
+    }
+    pub fn run_check(&mut self) -> IntErrStatus
+    {
+        match ((self.check)(),&self.status){
+            (true,IntErrStatus::OK) =>{
+                self.status = IntErrStatus::ERR;
+                (self.manage_fail)();
+                IntErrStatus::ERR
+            },
+            (true,IntErrStatus::ERR) =>{
+                IntErrStatus::ERR
+            },
+            (false,IntErrStatus::ERR) =>{
+                self.status = IntErrStatus::OK;
+                (self.reset_fail)();
+                IntErrStatus::OK
+            },
+            (false,IntErrStatus::OK) =>{
+                IntErrStatus::OK
+            },
+        }
+    }
+
+    pub fn get_description(&self) -> &String{
+        &self.description
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,37 +6,70 @@ use bus::BusComumication;
 use external::ExternalCheck;
 use internal::InternalCheck;
 
-pub struct ICSError{
+#[derive(Debug)]
+pub enum ErrorType {
+    INTERNAL,
+    EXTERNAL,
 }
 
 #[allow(unused)]
-pub struct ICS<MEX,INT,BUS> where 
-BUS: BusComumication<MEX>,INT:InternalCheck
+pub struct ICSError{
+    e_type: ErrorType,
+    e_desc: String,
+    e_id: usize,
+}
+
+#[allow(unused)]
+pub struct ICS<MEX,BUS> where 
+BUS: BusComumication<MEX>
 {
     bus: BUS,
-    int_vec: Vec<INT>,
+    int_vec: Vec<InternalCheck>,
     ext_vec: Vec<ExternalCheck<MEX,BUS>>,
 }
 
-pub trait ICST<MEX,BUS,I,ERR> 
-where BUS: BusComumication<MEX>, I: InternalCheck{
-    fn add_internal_check(&mut self, check: I) -> ();
-    fn add_external_check(&mut self, check: ExternalCheck<MEX,BUS>) -> ();
-    fn run_check(& mut self) -> Option<ERR>;
-}
-
-
 #[allow(unused)]
-impl<MEX,BUS,INT> ICST<MEX,BUS,INT,ICSError> for ICS<MEX,INT,BUS>
-where BUS: BusComumication<MEX>, INT: InternalCheck{
-    fn add_internal_check(&mut self, check: INT) -> ()
+impl<MEX,BUS> ICST<MEX,BUS,ICSError> for ICS<MEX,BUS>
+where BUS: BusComumication<MEX>{
+    fn add_internal_check(&mut self, check: InternalCheck) -> ()
     {
+        self.int_vec.push(check);
     }
     fn add_external_check(&mut self, check: ExternalCheck<MEX,BUS>) -> ()
     {
+        self.ext_vec.push(check);
     }
-    fn run_check(& mut self) -> Option<ICSError>
+    fn run_check(& mut self) -> Option<Vec<ICSError>>
     {
-        None
+        let mut res = vec![];
+        let mut i =0;
+
+        for int_check in &mut self.int_vec{
+            match int_check.run_check() {
+                internal::IntErrStatus::ERR => 
+                {
+                    res.push(ICSError{
+                            e_type: ErrorType::INTERNAL,
+                            e_desc: int_check.get_description().clone(),
+                            e_id: i,
+                        }
+                    );
+                },
+                _ => {},
+            }
+            i = i + 1;
+        }
+
+        match res.len(){
+            0 => None,
+            _ => Some(res),
+        }
     }
+}
+
+pub trait ICST<MEX,BUS,ERR> 
+where BUS: BusComumication<MEX>{
+    fn add_internal_check(&mut self, check: InternalCheck) -> ();
+    fn add_external_check(&mut self, check: ExternalCheck<MEX,BUS>) -> ();
+    fn run_check(& mut self) -> Option<Vec<ERR>>;
 }
