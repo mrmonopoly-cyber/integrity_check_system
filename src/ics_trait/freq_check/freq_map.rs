0765655464041784b8a088993f0438f72a352d98
Alberto Damo <alberto.damo@gmail.com>
2024-11-03 19:23:37 +0100

    stuff

--- src/ics_trait/freq_check/freq_map.rs
+++ src/ics_trait/freq_check/freq_map.rs
@@ -1,44 +1,64 @@
-use alloc::boxed::Box;
+use alloc::{boxed::Box, vec::Vec};
+use embedded_timers::{clock::Clock, instant::Instant};
 
 #[allow(unused)]
 #[derive(Debug)]
-struct FreqNode {
-    freq: usize,
+struct FreqNode<I: Instant> {
+    timeline: I,
     node_id: usize,
-    min_child: Option<Box<FreqNode>>,
-    max_child: Option<Box<FreqNode>>,
+    min_child: Option<Box<FreqNode<I>>>,
+    max_child: Option<Box<FreqNode<I>>>,
 }
 
 #[allow(unused)]
-impl FreqNode {
-    fn new(freq: usize,node_id:usize) -> Self{
-        Self{ freq, node_id, min_child: None, max_child: None }
+impl<I:Instant> FreqNode<I> {
+    fn new(timeline: I,node_id:usize) -> Self{
+        Self{ timeline, node_id, min_child: None, max_child: None }
     }
 
-    fn insert(&mut self,(freq,node_id):(usize,usize)) -> Result<(),()>{
-        fn check_child(se: &mut Option<Box<FreqNode>>,node_id:usize, freq: usize,) -> Result<(), ()>{
+    fn insert(&mut self,(timeline,node_id):(I,usize)) -> Result<(),()>{
+        fn check_child<I:Instant>(se: &mut Option<Box<FreqNode<I>>>,node_id:usize, timeline: I,)
+            -> Result<(), ()>{
             match se{
                 None => {
-                    *se = Some(Box::new(FreqNode::new(freq, node_id)));
+                    *se = Some(Box::new(FreqNode::new(timeline, node_id)));
                     Ok(())
                 },
                 Some(n) =>{
-                    n.insert((freq,node_id))
+                    n.insert((timeline,node_id))
                 },
             }
         }
-        if node_id < self.node_id{
-            check_child(&mut self.min_child, node_id, freq)
-        }else if node_id > self.node_id{
-            check_child(&mut self.max_child, node_id, freq)
+        if timeline < self.timeline{
+            check_child(&mut self.min_child, node_id, timeline)
+        }else if timeline > self.timeline{
+            check_child(&mut self.max_child, node_id, timeline)
         }else{
             Err(())
         }
     }
 
-    pub fn min(&self) -> Option<(usize,usize)>{
+    fn delete(&mut self,timeline:I){
+        fn check_child<I:Instant>(se: &mut Option<Box<FreqNode<I>>>, timeline: I,){
+            match se{
+                None => (),
+                Some(n) =>{
+                    n.delete(timeline)
+                },
+            }
+        }
+        if timeline > self.timeline{
+            check_child(&mut self.max_child, timeline)
+        }else if timeline < self.timeline{
+            check_child(&mut self.min_child, timeline)
+        }else{
+            todo!()
+        }
+    }
+
+    pub fn min(&self) -> Option<(I,usize)>{
         match &self.min_child{
-            None => Some((self.node_id, self.freq)),
+            None => Some((self.timeline,self.node_id)),
             Some(n) => n.min()
         }
     }
@@ -47,25 +67,62 @@ impl FreqNode {
 
 #[allow(unused)]
 #[derive(Debug)]
-pub struct FreqTree {
-    root: Option<FreqNode>,
+pub struct FreqTree<I,FClk>
+where 
+    I:Instant,
+    FClk: Clock<Instant = I>,
+{
+    root: Option<FreqNode<I>>,
+    clk: FClk
 }
 
 #[allow(unused)]
-impl FreqTree {
-    pub fn new() -> Self{
-        Self{root:None}
+impl<I,FClk> FreqTree<I,FClk>
+where 
+    I:Instant,
+    FClk: Clock<Instant = I>,
+{
+    pub fn new(clk: FClk) -> Self{
+        Self{root:None,clk}
     }
 
-    pub fn min(&self) -> Option<(usize,usize)>{
-        todo!();
+    pub fn min(&self) -> Option<(I,usize)>{
+        match &self.root{
+            None => None,
+            Some(n) => n.min()
+        }
     }
 
-    pub fn delete_node(&mut self, node_id: usize) -> Result<(),()>{
-        todo!();
+    pub fn delete_node(&mut self, timeline: I){
+        todo!()
     }
     
-    pub fn insert_node(&mut self,node_id: usize,freq:usize) -> Result<(),()>{
-        todo!();
+    pub fn insert_node(&mut self,node_id: usize,timeline:I) -> Result<(),()>{
+        match &mut self.root{
+            None => {
+                self.root = Some(FreqNode::new(timeline, node_id));
+                Ok(())
+            },
+            Some(n) =>n.insert((timeline,node_id)),
+        }
+    }
+
+    pub fn chek_freq<FClock>(&mut self,) -> Vec<usize>
+    {
+        let mut res = Vec::new();
+
+        loop {
+            match self.min(){
+                None => break,
+                Some((timeline,id)) => {
+                    if self.clk.now() < timeline{
+                        self.delete_node(timeline);
+                        res.push(id)
+                    }
+                },
+            }
+        }
+
+        res
     }
 }
